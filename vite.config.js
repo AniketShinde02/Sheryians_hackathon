d70b6c80d7d225ff63ee556c0809f1f5316f745e
Aniket Shinde <shindeaniket085@gmail.com>
2025-07-24 20:22:39 +0530

    Update: fixed Tailwind config, package updates, and CSS

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
+import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   server: {
     allowedHosts: [
       'rnatl-2401-4900-ac9d-92af-10a9-a28d-b2ab-16c8.a.free.pinggy.link'
